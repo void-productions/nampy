0e75ae41eaa31673a72648971e79dc22afede23e
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-28 23:01:03 +0200

    ...

--- nampy.py
+++ nampy.py
@@ -1,6 +1,8 @@
 import numpy as np
 from typing import Dict, List, Optional
 
+s = slice
+
 def zeros(shape: Dict[str, int]):
     axes = list(shape.keys())
     np_shape = list(shape.values())
@@ -19,7 +21,8 @@ class array:
         for (key, v) in kwargs.items():
             i = self._axes[key]
             l[i] = v 
-            axes_to_remove.append(i)
+            if isinstance(i, int):
+                axes_to_remove.append(i)
         axes_to_remove.sort(reverse=True)
         for i in axes_to_remove: del new_axes[i]
         return array(self._inner[tuple(l)], new_axes)
@@ -31,4 +34,5 @@ a = zeros(dict(x=3, y=2, z=5))
 print(a())
 print(a(x=1))
 print(a(x=1,y=1))
-print(a(x=1)(y=1))
+print(a(x=0)(y=1))
+print(a(x=0)(y=1)(z=s(1,3)))
