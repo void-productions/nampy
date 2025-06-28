9da24a59e05cb7856c2d84227927f619a58187c4
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-28 22:53:23 +0200

    ...

--- nampy.py
+++ nampy.py
@@ -13,16 +13,22 @@ class array:
         self._axes = dict(zip(axes, range(n)))
 
     def __call__(self, **kwargs):
+        new_axes = list(self._axes.keys())
         l = [np.s_[:]] * len(self._inner.shape)
+        axes_to_remove = []
         for (key, v) in kwargs.items():
             i = self._axes[key]
             l[i] = v 
-        return self._inner[tuple(l)]
+            axes_to_remove.append(i)
+        axes_to_remove.sort(reverse=True)
+        for i in axes_to_remove: del new_axes[i]
+        return array(self._inner[tuple(l)], new_axes)
 
     def __repr__(self):
         return repr(self._inner)
 
-a = zeros(dict(x=3, y=2))
+a = zeros(dict(x=3, y=2, z=5))
 print(a())
 print(a(x=1))
 print(a(x=1,y=1))
+print(a(x=1)(y=1))
